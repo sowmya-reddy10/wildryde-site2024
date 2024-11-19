1eaec79fd8a4e54f478ebbd56588005288a82bb0
sowmya-reddy10 <104055212+sowmya-reddy10@users.noreply.github.com>
2024-11-20 00:15:42 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_J3bvaWQSb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '15fejh1q6bah6uolo2k6b7nerc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: ' us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://4nuh2z799e.execute-api.us-east-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
